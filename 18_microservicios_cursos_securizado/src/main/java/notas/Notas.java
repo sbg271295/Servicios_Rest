d7ef7fc2d28d58351e1799022f1493e7abfffdd3
manana <manana@A13E41>
2024-09-05 13:55:28 +0200

    Ejercicio numero 1 de securizacion

---
+++ 18_microservicios_cursos_securizado/src/main/java/notas/Notas.java
@@ -0,0 +1,38 @@
+package notas;
+/*
+Para crear un microservicio springboot creamos un Spring StarterProject salvo que con 
+este tipo de microservicios, tenemos que modificar el packaging a jar y no a war 
+como se encontratraba antes.
+
+En primer punto, podemos observar que cambia el init, el servet initialazer se ha eliminado;
+
+La diferencia entre un servicio res y un microservicio es 0;
+
+Lo unico primordial es en el init indicar los packages que va a escanear
+
+
+No hay que tocar las versiones del tomcat
+
+
+---------------------------------
+
+Por otro lado, aquí se configura todo desde el src/main/resources
+
+en el apartado application.propierteis
+
+Hemos incluido dos apartados para estipular el puerto que vamos a usar
+y lo que se va a tener que poner en la pagina web
+
+-----------------------------------------
+
+Si pusamos el boton hexagonal que aparece arriba verde BOOT DASHBOARD, te va a aparecer
+una ventana en la que aparecen los proyectos de springboot que tienes activas.
+
+Y ves cual es su estado.
+
+-----------------------------------------
+
+*/
+public class Notas {
+
+}
